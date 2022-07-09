eb2f14e681eed19504296d2aaa2cbae20a874f5c
Chuanqi Chen <chuanqi.chen@gmail.com>
2022-07-09 13:14:31 -0500

    Update ros.sh

--- lab4/ros.sh
+++ lab4/ros.sh
@@ -3,33 +3,33 @@ sudo apt-get -qq update
 echo "initial update...done"
 sudo apt-get -qq install net-tools
 # ROS repostitories and Gazebo 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' -y
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2486D2DD83DB69272AFE98867170598AF249743
 sudo add-apt-repository http://packages.osrfoundation.org/gazebo/ubuntu-stable
 # install ros-noetic
-sudo apt update
+sudo apt update  -y
 sudo apt upgrade -y
-sudo apt install ros-noetic-desktop-full
+sudo apt install ros-noetic-desktop-full  -y
 # tools and libraries 
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential`
-sudo apt install python3-catkin-tools
-sudo apt install python3-osrf-pycommon
-sudo apt install libgtk-3-dev libncurses5-dev
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential  -y
+sudo apt install python3-catkin-tools  -y
+sudo apt install python3-osrf-pycommon  -y
+sudo apt install libgtk-3-dev libncurses5-dev  -y
 
 # software 
-sudo apt install git
-sudo snap install --classic code
-sudo apt install synaptic
+sudo apt install git  -y
+sudo snap install --classic code  -y
+sudo apt install synaptic  -y
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
-source /opt/ros/noetic/setup.bash`
+source /opt/ros/noetic/setup.bash
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc`
-sudo rosdep init
-rosdep update
+source ~/.bashrc  -y
+sudo rosdep init  -y
+rosdep update  -y
 
 cd
 mkdir catkin_ws
