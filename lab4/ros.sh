1d4a7b870de46bd956490f0218ccff827f480543
Harrison Luo <harrison.luo@gmail.com>
2022-09-20 16:27:23 -0700

    Correct a few errors

--- lab4/ros.sh
+++ lab4/ros.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
+# exit when any command fails
+set -e
+
 sudo apt-get -qq update
-echo "initial update...done"
 sudo apt-get -qq install net-tools
-# ROS repostitories and Gazebo 
+
+# ROS repostitories and Gazebo
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' -y
-sudo apt install curl
+sudo apt install curl -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2486D2DD83DB69272AFE98867170598AF249743
-sudo add-apt-repository http://packages.osrfoundation.org/gazebo/ubuntu-stable
+sudo add-apt-repository http://packages.osrfoundation.org/gazebo/ubuntu-stable -y
+
 # install ros-noetic
 sudo apt update  -y
 sudo apt upgrade -y
 sudo apt install ros-noetic-desktop-full -y
 sudo apt install ros-noetic-rosbash -y
-sudo apt install curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
 
 # tools and libraries 
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential  -y
@@ -30,29 +30,27 @@ sudo apt install libgtk-3-dev libncurses5-dev  -y
 
 # software 
 sudo apt install git  -y
-sudo snap install --classic code  -y
 sudo apt install synaptic  -y
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo apt install -y ./google-chrome-stable_current_amd64.deb
-
-source ~/.bashrc  -y
-
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/
-catkin_make
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-echo "Check ROS_PACKAGE_PATH below (should be /home/<pathtouser>/catkin_ws/src:/opt/ros/noetic/share)"
-echo $ROS_PACKAGE_PATH
-echo "git cloning repos"
 sudo apt-get -qq install python3-numpy python3-scipy python3-matplotlib ipython3 python3-pandas python3-sympy python3-nose -y
 sudo apt-get -qq install ros-noetic-slam-gmapping -y
 sudo apt-get -qq install ros-noetic-gmapping -y
 
-cd ~/catkin_ws/
-mkdir src
-cd src
+# Catkin and Texas Robotics Academy
+source /opt/ros/noetic/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
 catkin_init_workspace
 git clone https://github.com/Texas-Robotics-Academy/texas_robotics_academy.git
 cd ~/catkin_ws/
 catkin build
+source ~/catkin_ws/devel/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
+# Install Google Chrome and VSCode for x86 (AMC64) Ubuntu system
+echo "For x86 (AMD64) Ubuntu, install Google Chrom and VSCode as follows:"
+echo "  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+echo "  sudo apt install -y ./google-chrome-stable_current_amd64.deb"
+echo "  sudo snap install --classic code  -y"
+echo "For ARM64 Ubuntu, download and install VSCode at https://code.visualstudio.com/#alt-downloads"
+echo "Google Chrome does not have an ARM64 version as of 2022/09/20."
