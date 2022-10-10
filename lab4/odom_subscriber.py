6fdfbe27f5ec0d26cd2d85743826c698eeffea8d
Yichen Wang <vennahlot@users.noreply.github.com>
2022-10-10 15:15:11 -0700

    add boilerplate code in lab4

--- lab4/odom_subscriber.py
+++ lab4/odom_subscriber.py
@@ -1,12 +1,16 @@
 import rospy
+from nav_msgs.msg import Odometry
 
-
-def callback(data):
+def callback (data):
+    # TODO: Print pose info (1 line) Hint: rospy.loginfo() What's inside "data" variable?
+    # TODO: Print twist info (1 line)
     pass
 
 
-def subscriber():
-    pass
+def subscriber ():
+	rospy.init_node("odom_subscriber" , anonymous=True )
+    # TODO: Create a subscriber (1 line). http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29#rospy_tutorials.2FTutorials.2FWritingPublisherSubscriber.Writing_the_Subscriber_Node
+	rospy.spin()
 
 
 if __name__ == '__main__':
--- lab4/vel_publisher.py
+++ lab4/vel_publisher.py
@@ -1,14 +1,28 @@
-#!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
 
 
 def publisher():
-    pass
+    rospy.init_node("vel_publisher")
+    # TODO: Create publisher (1 line). http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29#rospy_tutorials.2FTutorials.2FWritingPublisherSubscriber.Writing_the_Publisher_Node
+    while not rospy.is_shutdown():
+        direction = input("f: forward, b: backward, l: left, r: right >")
+        vel = Twist()  # https://www.programcreek.com/python/example/70251/geometry_msgs.msg.Twist
+        if "f" in direction:
+            # TODO: Handle going forward by modifying vel (1 line) Hint: vel.linear.? = ?
+            pass
+        if "b" in direction:
+            # TODO: Handle going backward by modifying vel (1 line)
+            pass
+        if "l" in direction:
+            # TODO: Handle turning left by modifying vel (1 line)
+            pass
+        if "r" in direction:
+            # TODO: Handle turning right by modifying vel (1 line)
+            pass
+        if "q" in direction:
+            break
+        # TODO: Publish the vel as a message (1 line)
 
-
-if __name__ == '__main__':
-    try:
-        publisher()
-    except rospy.ROSInterruptException:
-        pass
+if __name__ == "__main__":
+    publisher()
